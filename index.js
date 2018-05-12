49a84ed15f4218bf766e20228c07fa18cf4a38f5
Alan Lee <alan@pointapp.org>
2018-05-11 18:49:29 -0700

    updated with consolelogging

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ app.post('/update', async (req, res) => {
 
     try {
         const update = await spawn('sudo', ['git', 'pull', '--rebase']);
-        
+        update.stdout.on('data', (data) => console.log(`${data}`))
+        update.stderr.on("data", data => console.log(`${data}`));
         update.on('close', async () => {
+            console.log('Update Done');
             const restart = await spawn('pm2', ['restart', '1']);
             
-            restart.on('close', () => res.status(200).send({ success: true }))
+            restart.on('close', () => {
+                console.log('Restarting!');
+                res.status(200).send({ success: true })
+            })
         })
     } catch (err) {
         res.status(400).send({ error: `Error pulling and restarting bot ${err}`})
