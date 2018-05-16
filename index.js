9a87960a1760f3ad2cedb29ea0169a8cc253ac7d
Alan Lee <alan@pointapp.org>
2018-05-15 22:26:55 -0700

    update script

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { spawn } = require('child_process');
+const { exec, spawn } = require('child_process');
 const path = require('path');
 const app = express();
 const PORT = 5000;
@@ -7,21 +7,19 @@ const PORT = 5000;
 app.post('/update', async (req, res) => {
 
     try {
-        const getIntoDir = await spawn('cd', ['/home/pjlangsam/Redwood']);
-        getIntoDir.stdout.on('data', (data) => console.log(`${data}`));
-
-        const update = await spawn('sudo', ['git', 'pull', '--rebase']);
-        update.stdout.on('data', (data) => console.log(`${data}`))
-        update.stderr.on("data", data => console.log(`${data}`));
-        update.on('close', async () => {
-            console.log('Update Done');
-            const restart = await spawn('pm2', ['restart', '1']);
+        const update = await exec('sudo git pull --rebase', { cwd: '/home/pjlangsam/Redwood' });
+        const restart = await spawn('pm2', ['restart', '1']);
+        // update.stdout.on('data', (data) => console.log(`${data}`))
+        // update.stderr.on("data", data => console.log(`${data}`));
+        // update.on('close', async () => {
+        //     console.log('Update Done');
+        //     const restart = await spawn('pm2', ['restart', '1']);
             
             restart.on('close', () => {
                 console.log('Restarting!');
                 res.status(200).send({ success: true })
             })
-        })
+        // })
     } catch (err) {
         res.status(400).send({ error: `Error pulling and restarting bot ${err}`})
     }
